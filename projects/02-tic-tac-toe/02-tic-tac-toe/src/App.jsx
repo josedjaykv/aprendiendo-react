38ea7c7e04886f8dab258962881d9638d208a25a
josedjaykv <josedjaykv@gmail.com>
2025-12-22 23:15:01 -0500

    Save to localStorage

--- projects/02-tic-tac-toe/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/02-tic-tac-toe/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import confetti from "canvas-confetti"
 import './App.css'
 import { Square } from './components/Square'
@@ -6,17 +6,27 @@ import { TURNS } from './constants'
 import { checkWinnerFrom, checkEndGame } from './logic/board'
 import { WinnerModal } from './components/WinnerModal'
 import { Board } from './components/Board'
+import {saveGameToStorage, resetGameStorage } from './logic/storage'
 
 function App() {
   // Estados
-  const [board, setBoard] = useState(Array(9).fill(null))  // Estado para saber en que estado esta cada cuadro del tablero
-  const [turn, setTurn] = useState(TURNS.X)  // Estado para saber en que turno estamos
+  const [board, setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem('board');
+    return boardFromStorage ? JSON.parse(boardFromStorage) : Array(9).fill(null);
+  })  // Estado para saber en que estado esta cada cuadro del tablero
+  
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem('turn');
+    return turnFromStorage ? turnFromStorage : TURNS.X;
+  })  // Estado para saber en que turno estamos
   const [winner, setWinner] = useState(null);
 
   const resetGame = () => {
     setBoard(Array(9).fill(null));
     setTurn(TURNS.X);
     setWinner(null);
+
+    resetGameStorage();
   }
 
   const updateBoard = (index) => {
@@ -32,6 +42,12 @@ function App() {
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
     setTurn(newTurn);
 
+    // Guardar partida en localStorage
+    saveGameToStorage({
+      board: newBoard,
+      turn: newTurn
+    })
+
     // Revisar si hay un ganador
     const newWinner = checkWinnerFrom(newBoard);
     if (newWinner){
@@ -41,6 +57,10 @@ function App() {
       setWinner(false);
     }
   }
+
+  useEffect(() => {
+    console.log('Hay ganador')
+  }, [winner])
 
   return (
     <main className='board'>
